616c56d745e9374d27afc6ef35220074459f071c
candidosales <candidosg@gmail.com>
2016-08-28 03:28:49 -0300

    mission add

--- frontend/app/scripts/controllers/mission/add.js
+++ frontend/app/scripts/controllers/mission/add.js
@@ -9,16 +9,14 @@
  */
 angular.module('frontendApp')
   .controller('MissionAddCtrl', function ($scope, $routeParams, $location, Mission) {
-
       $scope.mission = {
         mission: {
           name: "",
           description: "",
           goal:"",
           value:"",
-          start_date: new Date(),
-          end_date: new Date(),
-          user_id: $routeParams.userId
+          duration_month: 1,
+          user_id: $routeParams.user_id
         }
       }
 
