91a3a329a6e9aa3e3fe01b5fbc66102eb4316e71
candidosales <candidosg@gmail.com>
2016-08-28 00:09:22 -0300

    remove sidebar_menu, fix grunt serve

---
+++ frontend/app/scripts/controllers/user/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:UserLoginCtrl
+ * @description
+ * # UserLoginCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('UserLoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
